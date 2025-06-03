dcec6579a3f0e2433c4e7a87e6bfb69cfd9b349c
Naonao3 <naoto.n.2460@gmail.com>
2025-06-03 20:42:26 +0900

    next.config.ts訂正

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/profile' : '',
-  assetPrefix: isProd ? '/profile/' : '',
+  // basePath: isProd ? '/profile' : '',
+  // assetPrefix: isProd ? '/profile/' : '',
 };
 
 export default nextConfig;
